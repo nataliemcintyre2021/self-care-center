a8b87dadd84a0ec5a7650b6acbf64f818dac1507
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-05-30 14:40:15 -0700

    Clean up variable names

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ class Message {
     this.message = message;
   }
 }
-var newObject = new Message(currentQuote)
+var newQuote = new Message(currentQuote)
 
 var affirmations = [" I forgive myself and set myself free.",
 " I believe I can be all that I want to be." , " I am in the process of becoming the best version of myself.", " I have the freedom & power to create the life I desire.", " I choose to be kind to myself and love myself unconditionally.", " My possibilities are endless.", " I am worthy of my dreams.", " I am enough.", " I deserve to be healthy and feel good.", " I am full of energy and vitality and my mind is calm and peaceful.", " Every day I am getting healthier and stronger.", " I honor my body by trusting the signals that it sends me.", " I manifest perfect health by making smart choices."
@@ -103,8 +103,8 @@ function removeQuote(quote) {
 
 function addToFavoriteList() {
   event.preventDefault();
-  var newObject = new Message(currentQuote)
-  favoriteMessages.push(newObject);
+  var newQuote = new Message(currentQuote)
+  favoriteMessages.push(newQuote);
 }
 
 function deleteMessage() {
